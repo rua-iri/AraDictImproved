6820b52da94869389e60f9fc43a3bf9dbf39db87
rua-iri <rory.mcguigan@protonmail.com>
2023-02-01 14:47:07 +0000

    remove TODO comment

--- AraDictImproved/src/main/java/com/ruairi/SqlQuery.java
+++ AraDictImproved/src/main/java/com/ruairi/SqlQuery.java
@@ -8,9 +8,6 @@ import java.sql.ResultSet;
 
 public class SqlQuery {
     
-    // TODO maybe update the comparison tables tableAB, tableAC and tableBC
-    // so that they include the category id from their respective tables
-    // this might make searching through the database faster...
 
     private static final String jdbcUrl = SecretStuff.jdbcUrl.label;
     private static final String sQueryString = "SELECT CONCAT(prefixes.VOC_FORM, stems.VOC_FORM, suffixes.VOC_FORM) AS VOC_FORM, prefixes.GLOSS AS PRE_GLOSS, stems.GLOSS AS STE_GLOSS, suffixes.GLOSS AS SUF_GLOSS, CONCAT(prefixes.POS_NICE, ', ', stems.POS_NICE, ', ', suffixes.POS_NICE) AS POS, stems.ROOT, stems.MEASURE FROM stems INNER JOIN tableAB ON stems.CAT_ID=tableAB.stemCatId INNER JOIN prefixes ON tableAB.prefCatID=prefixes.CAT_ID INNER JOIN tableBC ON stems.CAT_ID=tableBC.stemCatID INNER JOIN suffixes ON tableBC.suffCatID=suffixes.CAT_ID WHERE BINARY prefixes.FORM='%1$s' AND BINARY stems.FORM='%2$s' AND BINARY suffixes.FORM='%3$s' AND EXISTS (SELECT * FROM tableAC WHERE tableAC.prefCatID=prefixes.CAT_ID AND tableAC.suffCatID=suffixes.CAT_ID); ";
