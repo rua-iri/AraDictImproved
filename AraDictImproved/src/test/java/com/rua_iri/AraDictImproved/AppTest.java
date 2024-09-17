9b5409ee9c8fdf84d9e3bbdf09938db8e75ce471
rua-iri <rory.mcguigan@protonmail.com>
2024-09-17 22:14:56 +0300

    add: unittest for env file

--- AraDictImproved/src/test/java/com/rua_iri/AraDictImproved/AppTest.java
+++ AraDictImproved/src/test/java/com/rua_iri/AraDictImproved/AppTest.java
@@ -3,7 +3,6 @@ package com.rua_iri.AraDictImproved;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.Assert.assertTrue;
 
 @SpringBootTest
 class AppTest {
--- AraDictImproved/src/test/java/com/rua_iri/AraDictImproved/QueryDBTest.java
+++ AraDictImproved/src/test/java/com/rua_iri/AraDictImproved/QueryDBTest.java
@@ -1,21 +1,35 @@
 package com.rua_iri.AraDictImproved;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.rua_iri.AraDictImproved.QueryDB;
 
-
-public class QueryDBTest {
+@SpringBootTest
+class QueryDBTest {
 
     @Test
-    public void testGenerateConnectionString() {
+    void testGenerateConnectionString() {
 
-        String expected = "";
         String actual = QueryDB.generateConnectionString();
-        
-        assertEquals(expected, actual);
+        System.out.println("\n\n\n" + actual + "\n\n\n");
+
+        // String regexPattern = "jdbc:mysql://localhost:+\\w/+\\w\\?user=+\\w&password=+\\w";
+        String regexPattern = "jdbc:mysql://localhost:\\w*/\\w*\\?user=\\w*&password=\\w*";
+
+        Pattern pattern = Pattern.compile(regexPattern, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(actual);
+
+        assertTrue(matcher.find());
+
+        assertNotEquals("jdbc:mysql://localhost:%s/%s?user=%s&password=%s", actual);
     }
-    
+
 }
