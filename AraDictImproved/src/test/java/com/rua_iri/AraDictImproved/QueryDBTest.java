e9c98292b181b160c3daf7095ef10e639705e814
rua-iri <rory.mcguigan@protonmail.com>
2024-09-17 22:27:34 +0300

    update: minor changes

--- AraDictImproved/src/test/java/com/rua_iri/AraDictImproved/QueryDBTest.java
+++ AraDictImproved/src/test/java/com/rua_iri/AraDictImproved/QueryDBTest.java
@@ -1,28 +1,33 @@
 package com.rua_iri.AraDictImproved;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.rua_iri.AraDictImproved.QueryDB;
-
 @SpringBootTest
 class QueryDBTest {
+
+    QueryDB queryDB;
+
+    @Before
+    void main() {
+        queryDB = new QueryDB();
+    }
 
     @Test
     void testGenerateConnectionString() {
 
         String actual = QueryDB.generateConnectionString();
 
-        String regexPattern = "jdbc:mysql://localhost:\\w*/\\w*\\?user=\\w*&password=\\w*";
-
-        Pattern pattern = Pattern.compile(regexPattern, Pattern.CASE_INSENSITIVE);
+        Pattern pattern = Pattern.compile(
+                "jdbc:mysql://localhost:\\w*/\\w*\\?user=\\w*&password=\\w*",
+                Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(actual);
 
         assertTrue(matcher.find());
