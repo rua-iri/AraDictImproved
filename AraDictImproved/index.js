1fae68240610da8eab2869893b4efc8213889df6
rua-iri <rory.mcguigan@protonmail.com>
2024-09-26 12:30:52 +0300

    update: rewrite codebase in js

---
+++ AraDictImproved/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/hello", (req, res) => {
+  console.log(req);
+  res.json({ hello: "world" });
+});
+
+
+
+module.exports = app;
