d5622fe9e60738e128d73c733e93b995b8424be7
rua-iri <rory.mcguigan@protonmail.com>
2024-09-27 11:50:14 +0300

    fix: put cors in correct location

--- AraDictImproved/index.js
+++ AraDictImproved/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const runAnalyser = require("./analyser");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/hello", (req, res) => {
   console.log(req.body);
--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-require("dotenv").config()
-const cors = require("cors")
+require("dotenv").config();
 const app = require("./AraDictImproved");
 
 const port = process.env.PORT || "3000";
 
-app.use(cors())
-
-console.log(`Server Running on port: ${port}`)
+console.log(`Server Running on port: ${port}`);
 
 app.listen(port);
