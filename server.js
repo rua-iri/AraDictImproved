b88f495bc5716ccddc7d0f058b33184f09740dd3
rua-iri <rory.mcguigan@protonmail.com>
2024-09-27 11:40:59 +0300

    add: cors

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require("dotenv").config()
+const cors = require("cors")
 const app = require("./AraDictImproved");
+
 const port = process.env.PORT || "3000";
+
+app.use(cors())
 
 console.log(`Server Running on port: ${port}`)
 
