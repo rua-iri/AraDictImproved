1f53cb010002adc669f59798fe6c648a1beef31c
rua-iri <rory.mcguigan@protonmail.com>
2024-10-19 16:32:40 +0300

    update: setup basic root db query api

--- RootAnalysis/index.js
+++ RootAnalysis/index.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const { runAnalyser } = require("./analyser");
+const { runQuery } = require("./queryDB");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("hello", (req, res) => {
+app.get("/hello", (req, res) => {
   console.log(req);
-  res.json({hello: "world"});
-})
+  res.json({ hello: "world" });
+});
 
+app.get("/root/:root", (req, res) => {
+  const root = req.params.root;
+  console.log(root);
+
+  const description = runQuery(root);
+
+  res.json(description);
+});
--- RootAnalysis/queryDB.js
+++ RootAnalysis/queryDB.js
@@ -5,7 +5,21 @@ const selectQuery = `
 SELECT lisan.root, lisan.desc
 FROM lisan
 WHERE lisan.root = @root;
-`
+`;
 
+async function runQuery(root) {
+  const db = await open({
+    filename: "data/lisanalarab.sqlite",
+    mode: sqlite.OPEN_READONLY,
+    driver: sqlite.Database,
+  });
 
+  const statement = await db.prepare(selectQuery);
+  const result = statement.all({
+    "@root": root,
+  });
 
+  return result;
+}
+
+module.exports = { runQuery };
