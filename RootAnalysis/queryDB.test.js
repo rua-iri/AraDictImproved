cb72e1d154de1ddd37d6df039af439b0623194de
rua-iri <rory.mcguigan@protonmail.com>
2024-10-21 14:34:50 +0300

    add: tests for root searching function

---
+++ RootAnalysis/queryDB.test.js
@@ -0,0 +1,34 @@
+const { runQuery } = require("./queryDB");
+
+test("Queries for root كحلب", async () => {
+  const data = await runQuery("كحلب");
+  expect(data).toEqual({
+    desc: "كَحْلَبٌ: اسم.",
+    root: "كحلب",
+  });
+});
+
+test("Queries for root ششا", async () => {
+  const data = await runQuery("ششا");
+  expect(data).toEqual({
+    root: "ششا",
+    desc: "ثعلب عن ابن الأَعرابي: الشَّشا الشِّيصُ.",
+  });
+});
+
+test("Queries for root كدع", async () => {
+  const data = await runQuery("كدع");
+  expect(data).toEqual({
+    root: "كدع",
+    desc: "كَدَعَه يَكْدَعُه كَدْعاً: دَفَعَه.",
+  });
+});
+
+
+test("Queries incorrectly for root كدع", async () => {
+    const data = await runQuery("ششا");
+    expect(data).not.toEqual({
+      root: "كدع",
+      desc: "كَدَعَه يَكْدَعُه كَدْعاً: دَفَعَه.",
+    });
+  });
