255c66aa592bcb3fc96e1265895ee9ccad6c4f19
rua-iri <rory.mcguigan@protonmail.com>
2025-06-11 17:16:19 +0300

    update: implement root querying from Hans Wehr MySQL DB

--- RootAnalysis/index.js
+++ RootAnalysis/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const { runQuery, runQueryMySQL } = require("./queryDB");
+const { runQuery } = require("./queryDB");
 const {
   Response200,
   Response404,
@@ -17,13 +17,30 @@ router.get("/hello", (req, res) => {
   res.status(200).send(new Response200({ hello: "world" }));
 });
 
-router.get("/:root", async (req, res) => {
+router.get("/lane/:root", async (req, res) => {
   try {
     const root = req.params.root;
     console.log(root);
 
-    // const rootData = await runQuery(root);
-    const rootData = await runQueryMySQL(root);
+    const rootData = await runQuery(root, "lane");
+
+    if (!rootData || rootData.length == 0) {
+      res.status(404).send(new Response404("No Roots found"));
+    } else {
+      res.status(200).send(new Response200(rootData));
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(new Response500());
+  }
+});
+
+router.get("/hans/:root", async (req, res) => {
+  try {
+    const root = req.params.root;
+    console.log(root);
+
+    const rootData = await runQuery(root, "hans");
 
     if (!rootData) {
       res.status(404).send(new Response404("No Roots found"));
--- RootAnalysis/queryDB.js
+++ RootAnalysis/queryDB.js
@@ -2,19 +2,23 @@ const sqlite = require("sqlite3").verbose();
 const { open } = require("sqlite");
 const mysql = require("mysql2/promise");
 
-const selectQuery = `
-SELECT lisan.root, lisan.desc
-FROM lisan
-WHERE lisan.root = @root;
-`;
-
-const selectQueryMySQL = `
-SELECT *
+const selectQueryLanesLexicon = `
+SELECT laneslexicon.root, 
+laneslexicon.word, 
+laneslexicon.meaning
 FROM laneslexicon
 WHERE laneslexicon.root = ?;
 `;
 
-async function runQueryMySQL(root) {
+const selectQueryHansWehr = `
+SELECT hanswehr.root, 
+hanswehr.word, 
+hanswehr.meaning 
+FROM hanswehr
+WHERE hanswehr.root = ?;
+`;
+
+async function runQuery(root, tableName) {
   const conn = await mysql.createConnection({
     host: "dictionary_db",
     port: "3306",
@@ -22,24 +26,20 @@ async function runQueryMySQL(root) {
     password: "password",
     database: "arabic_dictionaries",
   });
-  const [results, fields] = await conn.query(selectQueryMySQL, [root]);
+
+  let selectQuery;
+
+  if (tableName == "hans") {
+    selectQuery = selectQueryHansWehr;
+  } else if (tableName == "lane") {
+    selectQuery = selectQueryLanesLexicon;
+  } else {
+    throw new Error("Table name not found");
+  }
+
+  const [results, fields] = await conn.query(selectQuery, [root]);
 
   return results;
 }
 
-async function runQuery(root) {
-  const db = await open({
-    filename: "data/lisanalarab.sqlite",
-    mode: sqlite.OPEN_READONLY,
-    driver: sqlite.Database,
-  });
-
-  const statement = await db.prepare(selectQuery);
-  const result = statement.get({
-    "@root": root,
-  });
-
-  return result;
-}
-
-module.exports = { runQuery, runQueryMySQL };
+module.exports = { runQuery };
