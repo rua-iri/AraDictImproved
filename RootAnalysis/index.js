080b08635aed76086a0e3fdd1eae7e99950026ac
rua-iri <rory.mcguigan@protonmail.com>
2025-06-14 22:43:42 +0300

    add: endpoint to get root count

--- RootAnalysis/index.js
+++ RootAnalysis/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const { runQuery } = require("./queryDB");
+const { runQuery, runQueryCount } = require("./queryDB");
 const {
   Response200,
   Response404,
@@ -24,6 +24,24 @@ router.get("/:dict_name/:root", async (req, res) => {
     console.log(root);
 
     const rootData = await runQuery(root, dictName);
+
+    if (!rootData) {
+      res.status(404).send(new Response404("No Roots found"));
+    } else {
+      res.status(200).send(new Response200(rootData));
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(new Response500());
+  }
+});
+
+router.get("/:dict_name/count/:root/", async (req, res) => {
+  try {
+    const root = req.params.root;
+    const dictName = req.params.dict_name;
+
+    const rootData = await runQueryCount(root, dictName);
 
     if (!rootData) {
       res.status(404).send(new Response404("No Roots found"));
