ce613429b5ec6d0a1189237b4913b055a83ae55e
rua-iri <rory.mcguigan@protonmail.com>
2026-03-15 23:10:36 +0000

    fix: jest issues

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -5,6 +5,8 @@
 
 /** @type {import('jest').Config} */
 const config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
